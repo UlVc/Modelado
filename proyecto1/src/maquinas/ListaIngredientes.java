3607466c278523a2f6d947dce36b69ae80e9bd6e
Ulrich <ulvc@ciencias.unam.mx>
2019-09-21 21:04:11 -0500

    Se mejoró la identación

--- proyecto1/src/maquinas/ListaIngredientes.java
+++ proyecto1/src/maquinas/ListaIngredientes.java
@@ -1,141 +1,141 @@
 package src.maquinas;
 
 public class ListaIngredientes {
-    
-    private double cantidadDeAlmendras;
-    private double cantidadDeCacao;
-    private double cantidadDeLeche;
-    private double cantidadDeMantecaDeCacao;
 
-    private double cantidadDeGrenetina;
-    private double cantidadDeAgua;
-    private double cantidadDeMielDeMaiz;
-    private double cantidadDeMielDeAzucar;
-    private double cantidadDeMielDeSaborizanteAcido;
-    private double cantidadDeChamoy;
+	private double cantidadDeAlmendras;
+	private double cantidadDeCacao;
+	private double cantidadDeLeche;
+	private double cantidadDeMantecaDeCacao;
 
-    private double cantidadDeHarina;    
-    private double cantidadDeHuevo;    
-    private double cantidadDeSal;    
-    private double cantidadDeChispasDeChocolate;    
-    private double cantidadDeMermelada:
-    
-    public double getCantidadDeAlmendras() {
+	private double cantidadDeGrenetina;
+	private double cantidadDeAgua;
+	private double cantidadDeMielDeMaiz;
+	private double cantidadDeMielDeAzucar;
+	private double cantidadDeMielDeSaborizanteAcido;
+	private double cantidadDeChamoy;
+
+	private double cantidadDeHarina;
+	private double cantidadDeHuevo;
+	private double cantidadDeSal;
+	private double cantidadDeChispasDeChocolate;
+	private double cantidadDeMermelada:
+
+	public double getCantidadDeAlmendras() {
 		return cantidadDeAlmendras;
 	}
-    
+
 	public void setCantidadDeAlmendras(double cantidadDeAlmendras) {
 		this.cantidadDeAlmendras = cantidadDeAlmendras;
 	}
-    
+
 	public double getCantidadDeCacao() {
 		return cantidadDeCacao;
 	}
-    
+
 	public void setCantidadDeCacao(double cantidadDeCacao) {
 		this.cantidadDeCacao = cantidadDeCacao;
 	}
-    
+
 	public double getCantidadDeLeche() {
 		return cantidadDeLeche;
 	}
-    
+
 	public void setCantidadDeLeche(double cantidadDeLeche) {
 		this.cantidadDeLeche = cantidadDeLeche;
 	}
-    
+
 	public double getCantidadDeMantecaDeCacao() {
 		return cantidadDeMantecaDeCacao;
 	}
-    
+
 	public void setCantidadDeMantecaDeCacao(double cantidadDeMantecaDeCacao) {
 		this.cantidadDeMantecaDeCacao = cantidadDeMantecaDeCacao;
 	}
-    
+
 	public double getCantidadDeGrenetina() {
 		return cantidadDeGrenetina;
 	}
-    
+
 	public void setCantidadDeGrenetina(double cantidadDeGrenetina) {
 		this.cantidadDeGrenetina = cantidadDeGrenetina;
 	}
-    
+
 	public double getCantidadDeAgua() {
 		return cantidadDeAgua;
 	}
-    
+
 	public void setCantidadDeAgua(double cantidadDeAgua) {
 		this.cantidadDeAgua = cantidadDeAgua;
 	}
-    
+
 	public double getCantidadDeMielDeMaiz() {
 		return cantidadDeMielDeMaiz;
 	}
-    
+
 	public void setCantidadDeMielDeMaiz(double cantidadDeMielDeMaiz) {
 		this.cantidadDeMielDeMaiz = cantidadDeMielDeMaiz;
 	}
-    
+
 	public double getCantidadDeMielDeAzucar() {
 		return cantidadDeMielDeAzucar;
 	}
-    
+
 	public void setCantidadDeMielDeAzucar(double cantidadDeMielDeAzucar) {
 		this.cantidadDeMielDeAzucar = cantidadDeMielDeAzucar;
 	}
-    
+
 	public double getCantidadDeMielDeSaborizanteAcido() {
 		return cantidadDeMielDeSaborizanteAcido;
 	}
-    
+
 	public void setCantidadDeMielDeSaborizanteAcido(double cantidadDeMielDeSaborizanteAcido) {
 		this.cantidadDeMielDeSaborizanteAcido = cantidadDeMielDeSaborizanteAcido;
 	}
-    
+
 	public double getCantidadDeChamoy() {
 		return cantidadDeChamoy;
 	}
-    
+
 	public void setCantidadDeChamoy(double cantidadDeChamoy) {
 		this.cantidadDeChamoy = cantidadDeChamoy;
 	}
-    
+
 	public double getCantidadDeHarina() {
 		return cantidadDeHarina;
 	}
-    
+
 	public void setCantidadDeHarina(double cantidadDeHarina) {
 		this.cantidadDeHarina = cantidadDeHarina;
 	}
-    
+
 	public double getCantidadDeHuevo() {
 		return cantidadDeHuevo;
 	}
-    
+
 	public void setCantidadDeHuevo(double cantidadDeHuevo) {
 		this.cantidadDeHuevo = cantidadDeHuevo;
 	}
-    
+
 	public double getCantidadDeSal() {
 		return cantidadDeSal;
 	}
-    
+
 	public void setCantidadDeSal(double cantidadDeSal) {
 		this.cantidadDeSal = cantidadDeSal;
 	}
-    
+
 	public double getCantidadDeChispasDeChocolate() {
 		return cantidadDeChispasDeChocolate;
 	}
-    
+
 	public void setCantidadDeChispasDeChocolate(double cantidadDeChispasDeChocolate) {
 		this.cantidadDeChispasDeChocolate = cantidadDeChispasDeChocolate;
 	}
-    
+
 	public double getCantidadDeMermelada() {
 		return cantidadDeMermelada;
 	}
-    
+
 	public void setCantidadDeMermelada(double cantidadDeMermelada) {
 		this.cantidadDeMermelada = cantidadDeMermelada;
 	}
