1821a753f4c6d74c4c869e64c3d66d50bfdd7ba6
UlVc <ulvc@ciencias.unam.mx>
2019-09-21 20:38:17 -0500

    Se añadió la manera para cobrar

---
+++ proyecto1/src/maquinas/ListaIngredientes.java
@@ -0,0 +1,23 @@
+package src.maquinas;
+
+public class ListaIngredientes {
+    
+    private double cantidadDeAlmendras;
+    private double cantidadDeCacao;
+    private double cantidadDeLeche;
+    private double cantidadDeMantecaDeCacao;
+
+    private double cantidadDeGrenetina;
+    private double cantidadDeAgua;
+    private double cantidadDeMielDeMaiz;
+    private double cantidadDeMielDeAzucar;
+    private double cantidadDeMielDeSaborizanteAcido;
+    private double cantidadDeChamoy;
+
+    private double cantidadDeHarina;    
+    private double cantidadDeHuevo;    
+    private double cantidadDeSal;    
+    private double cantidadDeChispasDeChocolate;    
+    private double cantidadDeMermelada;    
+
+}
